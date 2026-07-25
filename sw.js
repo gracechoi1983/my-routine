56b96ad268d701411c8138ff2fbe40112d1feecc
Grace Choi <gracechoi1983@gmail.com>
2026-07-26 08:51:45 +1000

    브라우저 기본 팝업을 커스텀 모달로 교체

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "routine-v16";
+const CACHE = "routine-v17";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 const SORTABLE_CDN = "https://cdn.jsdelivr.net/npm/sortablejs@1.15.6/Sortable.min.js";
 self.addEventListener("install", e => {
